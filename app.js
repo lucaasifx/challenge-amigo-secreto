0e7b122579a65f0aa614eba1755c88dbd307e951
lucaasifx <lucaasifx@gmail.com>
2025-08-01 07:59:49 -0300

    :sparkles: feat: Listar amigos

--- app.js
+++ app.js
@@ -14,7 +14,19 @@ function adicionarAmigo() {
     if(validarEntrada(inputContent.value)) {
         secretFriends.push(inputContent.value);
         limparCampo(inputContent);
+        // só atualiza o DOM se um novo nome for adicionado à lista
+        listarAmigos();
     }
     else
         window.alert("Por favor, insira um nome.");
+}
+
+function listarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    let elementoListaAmigos = '';
+    secretFriends.forEach(amigo => {
+        elementoListaAmigos += `<li>${amigo}</li>`;
+    });
+    // dessa forma o DOM é manipulado uma única vez
+    listaAmigos.innerHTML = elementoListaAmigos;
 }
