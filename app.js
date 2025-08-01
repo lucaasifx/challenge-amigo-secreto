76a2f81b29ea2b04757f1000498d8ab63e644b12
lucaasifx <lucaasifx@gmail.com>
2025-08-01 07:22:45 -0300

    :sparkles: feat: Adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let secretFriends = [];
+
+
+function validarEntrada(input) {
+    return input == ''? false : true;
+}
+
+function limparCampo(campo) {
+    campo.value = '';
+}
+
+function adicionarAmigo() {
+    let inputContent = document.getElementById('amigo');
+    if(validarEntrada(inputContent.value)) {
+        secretFriends.push(inputContent.value);
+        limparCampo(inputContent);
+    }
+    else
+        window.alert("Por favor, insira um nome.");
+}
