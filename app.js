653f8755459943e411c10b6c781bc5b32698d22f
lucaasifx <lucaasifx@gmail.com>
2025-08-02 03:20:45 -0300

    :sparkles: feat: Sorteio de pessoas com nomes iguais

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+class Amigo {
+    constructor(nome, id) {
+        this.nome = nome;
+        this.id = id;
+    }
+};
+
 let secretFriends = [];
 let sorteados = [];
+let id = 1;
 
 function validarEntrada(input) {
     return input == ''? false : true;
@@ -12,7 +20,10 @@ function limparCampo(campo) {
 function adicionarAmigo() {
     let inputContent = document.getElementById('amigo');
     if(validarEntrada(inputContent.value)) {
-        secretFriends.push(inputContent.value);
+        let novoAmigo = new Amigo(inputContent.value, id);
+        //incrementa o id
+        id++;
+        secretFriends.push(novoAmigo);
         limparCampo(inputContent);
         // só atualiza o DOM se um novo nome for adicionado à lista
         listarAmigos();
@@ -25,7 +36,7 @@ function listarAmigos() {
     let listaAmigos = document.getElementById('listaAmigos');
     let elementoListaAmigos = '';
     secretFriends.forEach(amigo => {
-        elementoListaAmigos += `<li>${amigo}</li>`;
+        elementoListaAmigos += `<li>${amigo.nome}</li>`;
     });
     // dessa forma o DOM é manipulado uma única vez
     listaAmigos.innerHTML = elementoListaAmigos;
@@ -38,14 +49,26 @@ function gerarNumeroAleatorio() {
 
 function sortearAmigo() {
     let result = document.getElementById('resultado');
-    let amigoSorteado = secretFriends[gerarNumeroAleatorio()];
-    while(sorteados.includes(amigoSorteado) && sorteados.length != secretFriends.length)
-        amigoSorteado = secretFriends[gerarNumeroAleatorio()];
-    if(sorteados.length == secretFriends.length)
+    // se todos já foram sorteados
+    if(secretFriends.length == 0)
+        result.innerHTML = '';
+    else if(sorteados.length == secretFriends.length)
         result.innerHTML = 'Todos os amigos foram sorteados';
     else {
-        result.innerHTML = secretFriends.length > 0 ? `<li>O amigo secreto sorteado é: ${amigoSorteado}</li>`
-                                                    : '';
-        sorteados.push(amigoSorteado);
+        while(true) {
+            let foiSorteado = false;
+            let amigoSorteado = secretFriends[gerarNumeroAleatorio()];
+            sorteados.forEach(amigo => {
+                if(amigo.nome == amigoSorteado.nome) {
+                    if(amigo.id == amigoSorteado.id)
+                        foiSorteado = true;
+                }
+            });
+            if(!foiSorteado) {
+                result.innerHTML = `<li>O amigo secreto sorteado é: ${amigoSorteado.nome}</li>`
+                sorteados.push(amigoSorteado);
+                break;
+            }
+        }
     }
 }
